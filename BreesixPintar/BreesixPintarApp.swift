455133fcf4b908d7045b5e09ffa9333b76e6e014
Rangga Biner <ranggabiner@gmail.com>
2024-10-12 18:33:22 +0700

    Initial Commit

---
+++ BreesixPintar/BreesixPintarApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreesixPintarApp.swift
+//  BreesixPintar
+//
+//  Created by Rangga Biner on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BreesixPintarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
